2447919ca7ffe32e4ae3cf7ad7e200ae3576e1b1
Jon V <jon@agentrisk.com>
2022-04-17 16:24:26 -0700

    flask

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from flask import Flask, render_template
 import time
 from sds011 import *
 
 sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
-print("Starting sensor...")
-sensor.sleep(sleep=False)
-time.sleep(15)
 
-for i in range(0, 10):
+app = Flask(__name__)
+
+@app.route("/")
+def main():
     (pm25, pm10) = sensor.query()
-    print("pm25 ->", pm25, "pm10 ->", pm10)
-    time.sleep(5)
+    return render_template('index.html', pm25=pm25, pm10=pm10)
 
-sensor.sleep()
+
+
